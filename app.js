68f0057360b721822c39ce65a0634ce226a0c980
kamielnaeye <c0405572@student.viveshbo5.be>
2019-05-16 17:54:28 +0200

    database connectie gemaakt!

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
+var methotOverride = require('method-override');
+var mongoose = require('mongoose');
+var MongoStore = require('connect-mongo')(session);
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
+mongoose.connect("mongodb+srv://admin:Project123@projectkk-qrdxb.azure.mongodb.net/test?retryWrites=true", function(err) {
+    if (err) throw err;
+    console.log("Successfully connected to mongodb");
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,7 +24,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
