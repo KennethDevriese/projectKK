5b3752185b4bdd6e87db64627a5ac0388371eba2
kamielnaeye <c0405572@student.viveshbo5.be>
2019-06-06 16:26:44 +0200

    MQTT werkt

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var http = require('http').Server(app);
 var io = require('socket.io');
 const port = 2000;
 const socket = io(http);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,10 +24,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-//app.use('/home',indexRouter);
 app.use('/users', usersRouter);
 app.use('/support',supportRouter);
 app.use(express.static(path.join(__dirname,'node_modules/socket.io-client/dist/')));
+app.use(express.static(path.join(__dirname,'node_modules/mqtt/')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/support.js
+++ routes/support.js
@@ -3,15 +3,6 @@ var app = express();
 var router = express.Router();
 var mongoose = require('mongoose');
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://iot.eclipse.org');
-//mqtt
-client.on('connect', function () {
-      client.subscribe('presence', function (err) {
-        if (!err) {
-          client.publish('presence', 'Hello mqtt')
-        }
-      })
-});
      
 router.get('/', function(req, res, next) {
     res.render('support', { title: 'Support' });
